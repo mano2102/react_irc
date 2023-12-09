04f43955109b417a0aef859d4cb318e96c2e0214
mano2102 <112633959+mano2102@users.noreply.github.com>
2023-12-09 14:13:55 +0530

    Day -1 class exercise and class_work updated

---
+++ class_work/helloworld/src/App.js
@@ -0,0 +1,11 @@
+import Sample from './Sample'
+import './App.css'
+function App(){
+  return(
+    <div>
+      <Sample/>
+      <h1>Hello Buvi!</h1>
+    </div>
+  )
+}
+export default App
---
+++ class_work/helloworld/src/Sample.js
@@ -0,0 +1,7 @@
+function Sample(){
+    return(
+        <div>
+            <p>This is a sample component</p>
+        </div>
+    )
+}export default Sample
